06aaac8c19fa25fec3c9cc69cd08fa91f587cadb
Shubham Lingwal <95515278+Shubham12365@users.noreply.github.com>
2024-04-01 18:58:38 +0530

    Frontend

---
+++ Frontend/src/App.js
@@ -0,0 +1,44 @@
+import React, { useReducer } from "react";
+
+import LoginForm from "./Components/LoginForm";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import logo from "./logo.svg";
+
+import { BrowserRouter as Router } from "react-router-dom";
+import axios from "axios";
+
+import { useState } from "react";
+
+function App() {
+	const navigate = useNavigate();
+	const [css, setCss] = useState(false);
+	const [adcss, setadCss] = useState(false);
+	const [wtcss, setWtCss] = useState(false);
+	const [use, setUse] = useState();
+	const settle = (cust) => {
+		setUse(cust);
+		if (cust.role === 1) {
+			setadCss(true);
+		} else if (cust.role === 1) {
+			setCss(true);
+		} else {
+			setWtCss(true);
+		}
+	};
+
+	const x = () => {
+		setCss(false);
+		setadCss(false);
+		setWtCss(false);
+	};
+
+	return (
+		<div>
+			<Routes>
+				<Route path="/" element={<LoginForm fun={settle} />} />
+			</Routes>
+		</div>
+	);
+}
+
+export default App;
