598e53876e263e8f220c245eca204c80331984f9
Purnima3 <purnimakabadwal3@gmail.com>
2024-05-25 11:35:36 +0530

    added flask to integrate python code

--- Frontend/src/Components/Admin/Admin.jsx
+++ Frontend/src/Components/Admin/Admin.jsx
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import {useState} from 'react'
 import Navbar from "../Navbar/Navbar";
 import "./Admin.css";
 function Admin(props) {
+
 	const users = [
 		{ id: 1, name: "Patient 1" },
 		{ id: 2, name: "Patient 2" },
 		// Add more users as needed
 	];
+
+	//python flask code 
+	const [accuracy, setAccuracy] = useState(null);
+
+	useEffect(() => {
+        // Fetch the accuracy from the Flask API
+        fetch("/flask_api/ml")
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return res.json();
+            })
+            .then(data => setAccuracy(data.accuracy))
+            .catch(error => console.error('Error fetching accuracy:', error));
+    }, []); 
+
+
 	return (
 		<>
 			<Navbar d={props.data} />
 			<h1>Choose a Patient:</h1>
-			<div className="admin-container">
+			<p>
+			output: {accuracy !== null ? accuracy : "Loading..."}
+			</p>
+			{/* <div className="admin-container">
 				<div className="admin-content">
 					<ul className="user-list">
 						{users.map((user) => (
@@ -24,7 +47,7 @@ function Admin(props) {
 						))}
 					</ul>
 				</div>
-			</div>
+			</div> */}
 		</>
 	);
 }
