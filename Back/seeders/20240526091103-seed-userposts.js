490160547b8c03790064d24bcb6ef8c4d610fe30
Purnima3 <purnimakabadwal3@gmail.com>
2024-05-26 15:38:31 +0530

    Successfully entered data into the table

--- Back/seeders/20240526091103-seed-userposts.js
+++ Back/seeders/20240526091103-seed-userposts.js
@@ -13,22 +13,30 @@ module.exports = {
       fs.createReadStream(path.join(__dirname, 'output_data.csv'))
         .pipe(csv())
         .on('data', (data) => {
+          // Check if Date field is empty
+          if (!data.Date) {
+            console.log('Skipping row with empty Date field');
+            return;
+          }
+
           console.log('Raw data:', data);
-          console.log('Date value:', data.date);
+          console.log('Date value:', data.Date);
+
           results.push(data);
         })
         .on('end', () => {
           const records = results.map(row => {
-            const parsedDate = moment(row.date, 'YYYY-MM-DD');
+            // Parse Date field
+            const parsedDate = moment(row.Date, 'YYYY-MM-DD');
             if (!parsedDate.isValid()) {
-              console.error(`Invalid date format: ${row.date}`);
+              console.error(`Invalid date format: ${row.Date}`);
               return null; // Return null for invalid date
             }
             return {
-              userid: row.userid,
-              text: row.text,
+              userid: row.Userid,
+              text: row.Sentence,
               date: parsedDate.toDate(),
-              emotion: row.emotion,
+              emotion: row.Emotion,
               createdAt: new Date(),
               updatedAt: new Date()
             };
